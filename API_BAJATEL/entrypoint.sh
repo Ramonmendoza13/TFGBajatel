c7bf2634ddc4972124bd8d9723f731c56bca98ff
Ramón Mendoza Candelario <ramonm828@gmail.com>
2026-01-02 17:01:13 +0100

    añadir entrypoint.sh

---
+++ API_BAJATEL/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# entrypoint.sh
+
+# Crear SQLite si no existe
+touch database/database.sqlite
+
+# Limpiar caches de Laravel en runtime
+php artisan config:clear
+php artisan cache:clear
+php artisan route:clear
+php artisan view:clear
+
+# Arrancar servidor PHP apuntando a la carpeta public
+php -S 0.0.0.0:8000 -t public
