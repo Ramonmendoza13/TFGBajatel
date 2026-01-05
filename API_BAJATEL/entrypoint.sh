6e5f03e4b1768d3fe52368c176628d114957a9ee
Ramón Mendoza Candelario <ramonm828@gmail.com>
2026-01-05 12:55:26 +0100

    Cambiar a php artisan serve para procesar rutas de Swagger correctamente

--- API_BAJATEL/entrypoint.sh
+++ API_BAJATEL/entrypoint.sh
@@ -19,5 +19,5 @@ php artisan view:clear
 # Generar documentación de Swagger
 php artisan l5-swagger:generate
 
-# Arrancar servidor PHP apuntando a la carpeta public
-php -S 0.0.0.0:8000 -t public
+# Arrancar servidor Laravel (usa el router correcto)
+php artisan serve --host=0.0.0.0 --port=8000
